90227e2313a7d9a6dcc86aafbb42323aa2a0149b
its-sandy <santho8edu@gmail.com>
2020-12-02 14:11:16 +0530

    improve SRP Hash using torch matmul

--- SLIDE/cpp/srpHash.cpp
+++ SLIDE/cpp/srpHash.cpp
@@ -1,164 +1,42 @@
 #include <torch/extension.h>
 #include <ATen/Parallel.h>
-#include <chrono>
 #include <vector>
-#include <random>
-
-void reset_hashes(
-        torch::Tensor& nz_indices,
-        torch::Tensor& plus_mask,
-        torch::Tensor& minus_mask) {
-
-    int64_t L = nz_indices.size(0);
-    int64_t K = nz_indices.size(1);
-    int64_t nz_dim = nz_indices.size(2);
-    int64_t dim = plus_mask.size(2);
-
-    auto nz_indices_0 = nz_indices.accessor<int32_t, 3>(); // L x K x nz_dim
-    auto plus_mask_0 = plus_mask.accessor<bool, 3>(); // L x K x dim
-    auto minus_mask_0 = minus_mask.accessor<bool, 3>(); // L x K x dim
-
-    at::parallel_for(0, L * K, 0, [&](int64_t start, int64_t end) {
-        unsigned seed = std::chrono::system_clock::now().time_since_epoch().count() + start;
-        std::minstd_rand prng(seed);
-
-        for (int64_t ii = start; ii < end; ii++) {
-            int64_t l = ii/K, k = ii%K;
-            auto nz_indices_2 = nz_indices_0[l][k];
-            auto plus_mask_2 = plus_mask_0[l][k];
-            auto minus_mask_2 = minus_mask_0[l][k];
-            
-            int32_t tot_left = (dim<<1), nz_left = (nz_dim<<1);
-            int64_t ind = 0;
-            for(int32_t d=0; d<dim; d++) {
-                if(nz_left > 0) {
-                    int32_t rn = prng() % tot_left;
-                    if(rn < nz_left) {
-                        nz_indices_2[ind] = d;
-                        ind++;
-                        if(rn & 1) {
-                            plus_mask_2[d] = true;
-                            minus_mask_2[d] = false;
-                        }
-                        else {
-                            plus_mask_2[d] = false;
-                            minus_mask_2[d] = true;
-                        }
-                        nz_left -= 2;
-                    }
-                    else {
-                        plus_mask_2[d] = false;
-                        minus_mask_2[d] = false;
-                    }
-                }
-                else {
-                    plus_mask_2[d] = false;
-                    minus_mask_2[d] = false;
-                }
-                tot_left -= 2;
-            }
-        }
-    });
-}
-///////////////////////////////////////////////////////////////////////////////
-
-template <typename scalar_t>
-void get_hash_indices_dense_kernel(
-        torch::Tensor& hash_indices,
-        const torch::Tensor& in_values,
-        const torch::Tensor& nz_indices,
-        const torch::Tensor& plus_mask) {
-
-    int64_t batch_size = in_values.size(0);
-    int64_t L = nz_indices.size(0);
-    int64_t K = nz_indices.size(1);
-    int64_t nz_dim = nz_indices.size(2);
-
-    auto hash_indices_0 = hash_indices.accessor<int32_t, 2>(); // Batch x L
-    auto in_values_0 = in_values.accessor<scalar_t, 2>(); // Batch x dim
-    auto nz_indices_0 = nz_indices.accessor<int32_t, 3>(); // L x K x nz_dim
-    auto plus_mask_0 = plus_mask.accessor<bool, 3>(); // L x K x dim
-
-    at::parallel_for(0, batch_size * L, 0, [&](int64_t start, int64_t end) {
-        for (int64_t ii = start; ii < end; ii++) {
-            int64_t i = ii/L, l = ii%L;
-            auto in_values_1 = in_values_0[i];
-            auto nz_indices_1 = nz_indices_0[l];
-            auto plus_mask_1 = plus_mask_0[l];
-            
-            int32_t hash_index = 0;
-            for(int32_t k=0; k<K; k++) {
-                auto nz_indices_2 = nz_indices_1[k];
-                auto plus_mask_2 = plus_mask_1[k];
-                
-                scalar_t res = 0;
-                for(int64_t d=0; d<nz_dim; d++) {
-                    auto index = nz_indices_2[d];
-                    if(plus_mask_2[index])
-                        res += in_values_1[index];
-                    else
-                        res -= in_values_1[index];
-                }
-                if(res < 0)
-                    hash_index |= (1<<k);
-            }
-            hash_indices_0[i][l] = hash_index;
-        }
-    });
-}
-
-void get_hash_indices_dense(
-        torch::Tensor& hash_indices,
-        const torch::Tensor& in_values,
-        const torch::Tensor& nz_indices,
-        const torch::Tensor& plus_mask) {
-    
-    AT_DISPATCH_FLOATING_TYPES(
-        in_values.scalar_type(), "get_hash_indices_dense", [&] {
-            get_hash_indices_dense_kernel<scalar_t>(hash_indices, in_values, nz_indices, plus_mask);
-        });
-}
-///////////////////////////////////////////////////////////////////////////////
 
 template <typename scalar_t>
 void get_hash_indices_sparse_kernel(
         torch::Tensor& hash_indices,
         const torch::Tensor& in_values,
         const torch::Tensor& active_in_indices,
-        const torch::Tensor& plus_mask,
-        const torch::Tensor& minus_mask) {
-
+        const torch::Tensor& hash_vecs) {
+    // Some functionality of this function is similar to the sparse multiply
+    // operation that was implemented already. But we don't use that to avoid
+    // storing intermediate hash values separately, and to restrict number of parallel_fors used.
+    
     int64_t batch_size = in_values.size(0);
     int64_t active_in_dim = in_values.size(1);
-    int64_t L = plus_mask.size(0);
-    int64_t K = plus_mask.size(1);
+    int64_t L = hash_vecs.size(0);
+    int64_t K = hash_vecs.size(1);
 
     auto hash_indices_0 = hash_indices.accessor<int32_t, 2>(); // Batch x L
     auto in_values_0 = in_values.accessor<scalar_t, 2>(); // Batch x active_in_dim
     auto active_in_indices_0 = active_in_indices.accessor<int64_t,2>(); // Batch x active_in_dim
-    auto plus_mask_0 = plus_mask.accessor<bool, 3>(); // L x K x dim
-    auto minus_mask_0 = minus_mask.accessor<bool, 3>(); // L x K x dim
+    auto hash_vecs_0 = hash_vecs.accessor<scalar_t, 3>(); // L x K x dim
 
     at::parallel_for(0, batch_size * L, 0, [&](int64_t start, int64_t end) {
         for (int64_t ii = start; ii < end; ii++) {
             int64_t i = ii/L, l = ii%L;
             auto in_values_1 = in_values_0[i];
             auto active_in_indices_1 = active_in_indices_0[i];
-            auto plus_mask_1 = plus_mask_0[l];
-            auto minus_mask_1 = minus_mask_0[l];
+            auto hash_vecs_1 = hash_vecs_0[l];
             
             int32_t hash_index = 0;
             for(int32_t k=0; k<K; k++) {
-                auto plus_mask_2 = plus_mask_1[k];
-                auto minus_mask_2 = minus_mask_1[k];
+                auto hash_vecs_2 = hash_vecs_1[k];
                 
                 scalar_t res = 0;
                 for(int64_t d=0; d<active_in_dim; d++) {
                     auto index = active_in_indices_1[d];
-                    if(plus_mask_2[index])
-                        res += in_values_1[d];
-                    else if(minus_mask_2[index])
-                        res -= in_values_1[d];
+                    res += hash_vecs_2[index]*in_values_1[d]; // hash_vecs can also take values other than 0,1,-1
                 }
                 if(res < 0)
                     hash_index |= (1<<k);
@@ -172,19 +50,55 @@ void get_hash_indices_sparse(
         torch::Tensor& hash_indices,
         const torch::Tensor& in_values,
         const torch::Tensor& active_in_indices,
-        const torch::Tensor& plus_mask,
-        const torch::Tensor& minus_mask) {
+        const torch::Tensor& hash_vecs) {
     
     AT_DISPATCH_FLOATING_TYPES(
         in_values.scalar_type(), "get_hash_indices_sparse", [&] {
-            get_hash_indices_sparse_kernel<scalar_t>(hash_indices, in_values, active_in_indices, plus_mask, minus_mask);
+            get_hash_indices_sparse_kernel<scalar_t>(hash_indices, in_values, active_in_indices, hash_vecs);
         });
 }
 ///////////////////////////////////////////////////////////////////////////////
 
+template <typename scalar_t>
+void get_hash_indices_from_values_kernel(
+        torch::Tensor& hash_indices,
+        const torch::Tensor& hash_values) {
+
+    int64_t batch_size = hash_values.size(0);
+    int64_t L = hash_values.size(1);
+    int64_t K = hash_values.size(2);
+
+    auto hash_indices_0 = hash_indices.accessor<int32_t, 2>(); // Batch x L
+    auto hash_values_0 = hash_values.accessor<scalar_t, 3>(); // Batch x L x K
+
+    at::parallel_for(0, batch_size * L, 0, [&](int64_t start, int64_t end) {
+        for (int64_t ii = start; ii < end; ii++) {
+            int64_t i = ii/L, l = ii%L;
+            auto hash_values_2 = hash_values_0[i][l];
+            
+            int32_t hash_index = 0;
+            for(int32_t k=0; k<K; k++) {
+                if(hash_values_2[k] < 0)
+                    hash_index |= (1<<k);
+            }
+            hash_indices_0[i][l] = hash_index;
+        }
+    });
+}
+
+void get_hash_indices_from_values(
+        torch::Tensor& hash_indices,
+        const torch::Tensor& hash_values) {
+    
+    AT_DISPATCH_FLOATING_TYPES(
+        hash_values.scalar_type(), "get_hash_indices_from_values", [&] {
+            get_hash_indices_from_values_kernel<scalar_t>(hash_indices, hash_values);
+        });
+}
+
+
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-    m.def("reset_hashes", &reset_hashes, "reset_hashes");
-    m.def("get_hash_indices_dense", &get_hash_indices_dense, "get_hash_indices_dense");
     m.def("get_hash_indices_sparse", &get_hash_indices_sparse, "get_hash_indices_sparse");
+    m.def("get_hash_indices_from_values", &get_hash_indices_from_values, "get_hash_indices_from_values");
 }
--- SLIDE/srpHash.py
+++ SLIDE/srpHash.py
@@ -4,33 +4,26 @@ import srpHash_cpp
 
 
 class srpHashTable():
-    def __init__(self, K, L, dim, ratio):
+    def __init__(self, K, L, dim):
         self.K = K
         self.L = L
         self.dim = dim
-        self.nz_dim = math.ceil(dim / ratio)
-        
-        self.nz_indices = torch.empty([L, K, self.nz_dim], dtype=torch.int32)
-        self.plus_mask = torch.empty([L, K, dim], dtype=torch.bool)
-        self.minus_mask = torch.empty([L, K, dim], dtype=torch.bool)
         self.reset_hashes()
 
     def reset_hashes(self):
-        srpHash_cpp.reset_hashes(self.nz_indices,
-                                 self.plus_mask,
-                                 self.minus_mask)
+        self.hash_vecs = torch.rand([self.L*self.K, self.dim], dtype=torch.get_default_dtype()).round() * 2 -1
+        # self.hash_vecs = torch.rand([self.L*self.K, self.dim], dtype=torch.get_default_dtype()) - 0.5
 
     def get_hash_indices(self, in_values, active_in_indices=None):
         hash_indices = torch.empty([in_values.size(0), self.L], dtype=torch.int32)
         if active_in_indices is None:
-            srpHash_cpp.get_hash_indices_dense(hash_indices,
-                                               in_values,
-                                               self.nz_indices,
-                                               self.plus_mask)
+            with torch.no_grad():
+                hash_values = torch.mm(in_values, self.hash_vecs.t())
+                hash_values = hash_values.reshape(hash_values.size(0), self.L, self.K)
+            srpHash_cpp.get_hash_indices_from_values(hash_indices, hash_values)
         else:
             srpHash_cpp.get_hash_indices_sparse(hash_indices,
                                                 in_values,
                                                 active_in_indices,
-                                                self.plus_mask,
-                                                self.minus_mask)
+                                                self.hash_vecs.reshape(self.L, self.K, self.dim))
         return hash_indices
