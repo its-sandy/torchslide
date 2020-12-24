177b5229a1fa9cbaad7bd109b485f77c6e7d8d02
its-sandy <santho8edu@gmail.com>
2020-12-24 20:07:40 +0530

    bucketsTable.py merged from experiments

--- SLIDE/bucketsTable.py
+++ SLIDE/bucketsTable.py
@@ -10,19 +10,40 @@ class bucketsTable():
         self.buckets = torch.zeros([self.L, self.num_buckets, self.bucket_size], dtype=torch.int32)
         # bucket_counts holds the total number of nodes assigned to that bucket (can exceed bucket_size)
         self.bucket_counts = torch.zeros([self.L, self.num_buckets], dtype=torch.int32)
+        # num_nodes holds total number of nodes filled into the buckets (can exceed num_buckets x bucket_size)
         self.num_nodes = 0
     
-    def fill_buckets(self, hash_indices):
-        # Works best when L is larger than num_threads (to avoid inconsistencies because of race conditions)
-        # Ideally L is a multiple of num_threadss
+    def fill_buckets_FIFO(self, hash_indices):
         self.bucket_counts.fill_(0)
         self.num_nodes = hash_indices.size(0)
-        bucketsTable_cpp.fill_buckets(hash_indices, self.buckets, self.bucket_counts)
+        bucketsTable_cpp.fill_buckets_FIFO(hash_indices, self.buckets, self.bucket_counts)
 
-    def sample_nodes(self, hash_indices, active_nodes=None):
-        # either active_nodes should be partially pre filled with the required label nodes
-        # else, figure a way to send lists to pytorch c++ extensions
-        # maybe look into nested tensor
-        if required_nodes > self.num_nodes:
-            raise Exception
-        bucketsTable_cpp.sample_nodes_vanilla(self.buckets, self.bucket_counts, hash_indices)
+    def fill_buckets_reservoir_sampling(self, hash_indices):
+        self.bucket_counts.fill_(0)
+        self.num_nodes = hash_indices.size(0)
+        bucketsTable_cpp.fill_buckets_reservoir_sampling(hash_indices, self.buckets, self.bucket_counts)
+
+    def generate_randperm_nodes(self):
+        # This has considerable latency
+        self.randperm_nodes = torch.randperm(self.num_nodes, dtype=torch.int32)
+    
+    def sample_nodes_vanilla(self, hash_indices, sampled_nodes, presample_counts=None):
+        # hash_indices = [batch_size x L] (dtype=torch.int32)
+        # sampled_nodes = [batch_size x sample_size] (dtype=torch.int32)
+        # presample_counts = [batch_size] (dtype=torch.int32)
+        # If some nodes are always to be included in the sample, they should be
+        # in the prefix of sampled_nodes, and their counts in presample_counts
+        assert sampled_nodes.size(1) <= self.num_nodes, "Trying to sample more nodes than available in bucket"
+        if presample_counts is None:
+            presample_counts = torch.zeros([sampled_nodes.size(0)], dtype=torch.int32)
+        
+        bucketsTable_cpp.sample_nodes_vanilla(sampled_nodes,
+                                              presample_counts,
+                                              hash_indices,
+                                              self.buckets,
+                                              self.bucket_counts,
+                                              self.randperm_nodes)
+
+    def clear_buckets(self):
+        self.bucket_counts.fill_(0)
+        self.num_nodes = 0
